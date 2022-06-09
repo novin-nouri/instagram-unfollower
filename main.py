f254e29305e90ee559b5b620bd1498b69259be90
Noviin78 <novinnouri@gmail.com>
2022-06-09 09:39:55 +0430

    without bug

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from print import print_, print_find
 import time
 import random
+import pyautogui
 
 
 class Instabot:
@@ -18,6 +19,7 @@ class Instabot:
         smsf = self.driver.find_element_by_xpath("//input"
                                                  "[@name='verificationCode']")
         smsf.send_keys(sms)
+        time.sleep(random.randint(2, 3))
         self.driver.find_element_by_xpath("//button[@type='button']").click()
         # Save pass or not now
         time.sleep(random.randint(8, 10))
@@ -28,7 +30,11 @@ class Instabot:
         # The desired page
         self.driver.get("https://www.instagram.com/{}/".format(self.id_taraf))
         time.sleep(random.randint(2, 5))
-        self.driver.find_element_by_xpath("//button[@type='button']").click()
+        # agar error dar vorod etelat shdo in ra comment kon
+        # bayad ye if barash dorost konam khodesh tashkhis  bede
+        # self.driver.find_element_by_xpath("//button[@type='button']").click()
+
+        time.sleep(random.randint(2, 4))
         user_name = self.driver.find_element_by_xpath("//input"
                                                       "[@name='username']")
         user_name.send_keys(self.username)
@@ -43,20 +49,24 @@ class Instabot:
 
     def following_section(self):
         time.sleep(4)
-        self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/" 
-                                          "div[1]/div/div/div[1]/div[1]/"
-                                          "section/main/div/header/section/"
-                                          "ul/li[3]/a/div").click()
+        # self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/"
+        #                                   "div[1]/div/div/div[1]/div[1]/"
+        #                                   "section/main/div/header/section/"
+        #                                   "ul/li[3]/a/div").click()
+        self.driver.find_element_by_xpath("//a[contains(@href, "
+                                           "'/following')]").click()
         scrol_aval1_dovom2 = 1  # tozih dar paein bakhshe _get_names()
         following = self._get_names(scrol_aval1_dovom2)  # chon scrol following ba follower fargh dare (xpath) an
         return following
 
     def followers_section(self):
         time.sleep(4)
-        self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/"
-                                          "div[1]/div/div/div[1]/div[1]/"
-                                          "section/main/div/header/"
-                                          "section/ul/li[2]/a/div").click()
+        # self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/"
+        #                                   "div[1]/div/div/div[1]/div[1]/"
+        #                                   "section/main/div/header/"
+        #                                   "section/ul/li[2]/a/div").click()
+        self.driver.find_element_by_xpath("//a[contains(@href,"
+                                           " '/followers')]").click()
         time.sleep(random.randint(5, 7))
         scrol_aval1_dovom2 = 2
         followers = self._get_names(scrol_aval1_dovom2)
@@ -80,37 +90,48 @@ class Instabot:
 
     # scrol and get page id
     def _get_names(self, scrol_box_tedad):
-        scrol_following = "//*[@id='mount_0_0_zp']/div/div[1]/div/" \ 
-                          "div[2]/div/div/div[1]/div/div[2]/div/" \ 
-                          "div/div/div/div/div/div/div[3]"
-        scrol_followers = "//*[@id='mount_0_0_zp']/div/div[1]/div/" \ 
-                          "div[2]/div/div/div[1]/div/div[2]/div/" \ 
-                          "div/div/div/div/div/div/div[2]"
-        close_box = "/html/body/div[1]/div/div[1]/div/div[2]/div/" \ 
-                    "div/div[1]/div/div[2]/div/div/div/div/div/div/" \ 
-                    "div/div[1]/div/div[3]/div/button/div/svg/polyline"
+        # scrol_following = "//*[@id='mount_0_0_zp']/div/div[1]/div/" \
+        #                   "div[2]/div/div/div[1]/div/div[2]/div/" \
+        #                   "div/div/div/div/div/div/div[3]"
+        # scrol_followers = "//*[@id='mount_0_0_zp']/div/div[1]/div/" \
+        #                   "div[2]/div/div/div[1]/div/div[2]/div/" \
+        #                   "div/div/div/div/div/div/div[2]"
+        # clos_box = "//*[@id='mount_0_0_Xt']/div/div[1]/div/div[2]/div/" \
+        #                                  "div/div[1]/div/div[2]/div/div/" \
+        #                                  "div/div/div/div/div/div[1]/div/" \
+        #                                  "div[3]/div/button"
         time.sleep(random.randint(3, 5))
         # morabae follow va following
         # scro_aval... : in bakhsh baraye ine ke XPATH scroll box bakhsh following va followers fargh darad.agar 1 bashad yani following va agar 2 bashad yani follower
         if scrol_box_tedad == 1:
-            scroll_box = self.driver.find_element_by_xpath(scrol_following)
+            # scroll_box = self.driver.find_element_by_xpath(scrol_following)
+            scroll_box = self.driver.find_element_by_xpath("//div"
+                                                           "[@class='_aano']")
         elif scrol_box_tedad == 2:
-            scroll_box = self.driver.find_element_by_xpath(scrol_followers)
-        # scroll_box = self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[3]")
+            # scroll_box = self.driver.find_element_by_xpath(scrol_followers)
+            scroll_box = self.driver.find_element_by_xpath("//div"
+                                                           "[@class='_aano']")
 
+        time.sleep(5)
+        # height variable
         last_ht, ht = 0, 1
         while last_ht != ht:
             last_ht = ht
-            time.sleep(2)
-            ht = self.driver.execute_script("""
-                   arguments[0].scrollTo(0,arguments[0].scrollHeight);
-                   return arguments[0].scrollHeight;
-                   """, scroll_box)
+            time.sleep(random.randint(2, 3))
+            # scroll down and retrun the height of scroll (JS script)
+            ht = self.driver.execute_script(""" 
+                arguments[0].scrollTo(0, arguments[0].scrollHeight);
+                return arguments[0].scrollHeight; """, scroll_box)
+
+        time.sleep(random.randint(3, 5))
         print("\n\n-Please wait a moment...\n")
+        # links = scroll_box.find_elements_by_tag_name("a")
         links = scroll_box.find_elements_by_tag_name("a")
+        time.sleep(random.randint(2, 3))
         names = [name.text for name in links if name.text != '']
-        # zarbdar   /html/body/div[4]/div/div/div[1]/div/div[2]/button
-        self.driver.find_element_by_xpath(close_box).click()
+        # zarbdar   ke az Esc estefade mikonim
+        time.sleep(2)
+        pyautogui.press('esc')
         return names
 
     def quit(self):
